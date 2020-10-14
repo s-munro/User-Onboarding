f80d6300d251f37a9450ccdabb500d0be7308d31
s-munro <68410363+s-munro@users.noreply.github.com>
2020-10-14 18:20:32 -0400

    added Form.js, rendered content

---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './App.css';
+
+export default function UserForm(props) {
+
+
+
+  return (
+<form className='form container'>
+<div>
+<h2>Add a User</h2>
+<label className='label'>
+  Name
+  <input name="username" type="text">
+  </input>
+</label>
+<br />
+
+<label className='label'>
+  Email
+  <input name="email" type="email">
+  </input>
+</label>
+<br />
+
+<label className='label'>
+  Password
+  <input name="password" type="text">
+  </input>
+</label>
+<br />
+
+<label className='label'>
+    I Have Read the Terms of Service
+    <input type="checkbox">
+    </input>
+</label>
+<br />
+<button>Submit</button>
+</div>
+{/* - [ ] Name
+- [ ] Email
+- [ ] Password
+- [ ] Terms of Service (checkbox)
+- [ ] A Submit button to send our form data to the server. */}
+
+</form>
+
+
+  )
+}
